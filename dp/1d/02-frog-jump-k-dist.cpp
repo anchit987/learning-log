b02d5bc4439e709016395905d772ca995be3bd16
anchit987 <anchit987@gmail.com>
2026-04-03 17:50:02 +0530

    revision - add tabulation for frog-jump-k

--- dp/1d/02-frog-jump-k-dist.cpp
+++ dp/1d/02-frog-jump-k-dist.cpp
@@ -47,12 +47,26 @@ int calMinEnergyRecursively(vector<int> &h, int idx, int k, vector<int> &dp)
     return dp[idx] = minEnergy;
 }
 
+int solveTabulation(int n, int k, vector<int>& h) {
+    vector<int> dp(n, INT_MAX);
+    dp[0] = 0;
+
+    for(int i = 1; i<n; i++) {
+        for(int j  = max(0, i-k); j<i; j++) {
+            int jump = abs(h[i] - h[j]) + dp[j];
+            dp[i] = min(dp[i], jump);
+        }
+    }
+
+    return dp[n-1];
+}
+
 int calMinEnergy(int n, int k, vector<int> &h)
 {
     // Memoization
     vector<int> dp(n, -1);
     int memoAns = calMinEnergyRecursively(h, n - 1, k, dp);
-    return memoAns;
+    return solveTabulation(n, k, h);
 }
 
 int main()
